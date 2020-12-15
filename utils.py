1666365abbfab2748ecab83baee518f0271501f9
Stuart Axon <stuaxo2@yahoo.com>
2020-12-15 14:09:53 +0000

    Reformat util with Black.

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def strtobool(text: Union[str, bool]):
     if text is True or text is False:
         return text
 
-    if text.lower() in ['y', 'yes', 'on', 'true', '1']:
+    if text.lower() in ["y", "yes", "on", "true", "1"]:
         return True
 
     return False
